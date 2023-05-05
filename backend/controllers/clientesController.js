6c0347f234d3c6ad4f6350561bfccd8c2602461e
EdithRPotes <110207483+EdithRPotes@users.noreply.github.com>
2023-05-05 16:30:59 -0500

    Modelo,routing,controlador de cliente configurado y se utilizo crud

--- backend/controllers/clientesController.js
+++ backend/controllers/clientesController.js
@@ -1,18 +1,97 @@
 import Clientes from "../models/Clientes.js";
 
 
-const agregarCliente = (req, res) => {
+const agregarCliente = async (req, res) => {
 
     const cliente = new Clientes(req.body); // instancia del objeto de cliente
-    
+    cliente.usuario = req.usuario._id; // se trae el id del usuario que fue previamente registrado
     try {
-        
+        const clienteAlmacenado  = await  cliente.save(); // guardar en la BD
+        res.json(clienteAlmacenado);
     } catch (error) {
         console.log(error)
     }
 };
-const obtenerClientes = (req, res) => {};
+const obtenerClientes = async (req, res) => {
+    // se filtran los clientes que hayan manipulado un usuario que ha iniciado sesión
+    const clientes = await Clientes.find()
+    .where("usuario")
+    .equals(req.usuario);
+
+    res.json(clientes);   
+};
+
+
+//Obtener cliente en especifico 
+const obtenerCliente = async (req, res) => { 
+    const {id} = req.params;
+    const cliente = await Clientes.findById(id);
+
+    if(!cliente){
+      return  res.status(404).json({msg: 'No se ha encontrado'})
+    }
+
+    // validar si el usuario que ingreso al cliente es el que esta accediendo a la información 
+    if(cliente.usuario._id.toString() !== req.usuario._id.toString()){
+       return res.json({msg:'Acción invalidada'})
+    }
+
+   
+        res.json(cliente);
+    
+};  
+
+const actualizarCliente = async (req, res) => {
+    const {id} = req.params;
+    const cliente = await Clientes.findById(id);
+    
+    if(!cliente){
+      return  res.status(404).json({msg: 'No se ha encontrado'})
+    }
+
+    // validar si el usuario que ingreso al cliente es el que esta accediendo a la información 
+    if(cliente.usuario._id.toString() !== req.usuario._id.toString()){
+       return res.json({msg:'Acción invalidada'})
+    }
+
+    //Actualizar Cliente  leemos datos y se guardan 
+    cliente.nombre = req.body.nombre || cliente.nombre; // en caso de que un valor no se actualice mantiene el valor que tenia previamente 
+    cliente.documento = req.body.documento || cliente.documento;
+    cliente.email = req.body.email || cliente.email;
+    cliente.telefono = req.body.telefono || cliente.telefono;
+    cliente.direccion = req.body.direccion || cliente.direccion;
+    cliente.licencia = req.body.licencia || cliente.licencia;
+
+    try {
+        const clienteActualizado = await cliente.save();  // guarda los cambios en la BD 
+        res.json(clienteActualizado);
+    } catch (error) {
+        console.log(error);
+    }
+};   
+const eliminarCliente = async (req, res) => {
+    const {id} = req.params;
+    const cliente = await Clientes.findById(id);
+    
+    if(!cliente){
+      return  res.status(404).json({msg: 'No se ha encontrado'})
+    }
+
+    // validar si el usuario que ingreso al cliente es el que esta accediendo a la información 
+    if(cliente.usuario._id.toString() !== req.usuario._id.toString()){
+       return res.json({msg:'Acción invalidada'})
+    }
+
+    try {
+        await cliente.deleteOne();
+        res.json({msg: "Cliente Eliminado"})
+    } catch (error) {
+        console.log(error)
+    }
+
+
+};   
 
 
 
-export {agregarCliente, obtenerClientes};
+export {agregarCliente, obtenerClientes, obtenerCliente, actualizarCliente, eliminarCliente};
--- backend/routes/clientesRoutes.js
+++ backend/routes/clientesRoutes.js
@@ -1,10 +1,20 @@
 import express from 'express';
 const router = express.Router();
-import { agregarCliente, obtenerClientes} from '../controllers/clientesController.js'
+import { agregarCliente, obtenerClientes, obtenerCliente, actualizarCliente, eliminarCliente} from '../controllers/clientesController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 //aqui se define el cuerpo de los diferentes rutas (endpoint)
 router.route('/')
-    .post(agregarCliente) //Registro los pacientes 
-    .get(obtenerClientes) //lista los clientes 
+    .post(checkAuth, agregarCliente) //Registro los pacientes 
+    .get(checkAuth, obtenerClientes) //lista los clientes 
+
+
+//obtener un cliente en especifico
+router
+    .route("/:id") // id del cliente
+    .get(checkAuth, obtenerCliente)
+    .put(checkAuth, actualizarCliente)
+    .delete(checkAuth, eliminarCliente)
+
 
 export default router;
